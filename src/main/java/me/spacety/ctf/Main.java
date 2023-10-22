0ef54e17914d82a82d02e5c7c6757b092b8cab83
peselis <peselis20100@gmail.com>
2023-10-22 12:32:20 +0200

    incommit

---
+++ src/main/java/me/spacety/ctf/Main.java
@@ -0,0 +1,16 @@
+package me.spacety.ctf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
